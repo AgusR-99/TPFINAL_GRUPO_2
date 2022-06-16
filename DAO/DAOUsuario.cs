82a8fdce26271fd88126972006d3e555120581d8
Pedro <pedrogabe@gmail.com>
2022-06-16 18:52:15 -0300

    Capa DAO

---
+++ DAO/DAOUsuario.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Entidades;
+
+namespace DAO
+{
+    public static class DAOUsuario
+    {
+        public static int Login(ref Usuario usuario)
+        {
+            try
+            {
+                var parameters = GetLoginParameters(usuario);
+                DataTable resultado = DB.ObtenerTabla("Usuario", "SP_Login", parameters, true);
+
+                if (resultado == null) return -1;
+                if (resultado.Rows.Count == 0) return 0;
+
+                usuario = GetUsuarioFromDataRow(resultado.Rows[0]);
+                return 1;
+            }
+            catch
+            {
+                return -1;
+            }
+
+        }
+
+        private static List<SqlParameter> GetLoginParameters(in Usuario usuario)
+        {
+            return new List<SqlParameter>()
+            {
+                new SqlParameter("username", usuario.getUsername()),
+                new SqlParameter("contrasena", usuario.getContraseña())
+            };
+        }
+
+        private static Usuario GetUsuarioFromDataRow(in DataRow row)
+        {
+            return new Usuario(
+                (string)row["Username"],
+                (string)row["Email"],
+                (bool)row["Administrador"],
+                row["Descripcion"] == DBNull.Value ? null : (string)row["Descripcion"]);
+        }
+    }
+}
---
+++ DAO/DB.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+using Entidades;
+
+namespace DAO
+{
+    static class DB
+    {
+        private static string connString = "Data Source=localhost\\sqlexpress; Initial Catalog=TIF2022_PROG_DRAFT; Integrated Security=True";
+        
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connString);
+        }
+
+        private static SqlDataAdapter GetAdapter(SqlCommand cmd)
+        {
+            SqlDataAdapter adaptador;
+            try
+            {
+                adaptador = new SqlDataAdapter(cmd);
+                return adaptador;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        private static SqlCommand GetCommand(in string query, in List<SqlParameter> parameters, bool isSP = false)
+        {
+            var cmd = new SqlCommand(query, GetConnection());
+            cmd.CommandType = isSP ? CommandType.StoredProcedure : CommandType.Text;
+            
+            AddParameters(ref cmd, parameters);
+            return cmd;
+        }
+
+        public static void AddParameters(ref SqlCommand cmd, in List<SqlParameter> parameters)
+        {
+            if (parameters!=null)
+            {
+                foreach(SqlParameter parameter in parameters)
+                {
+                    cmd.Parameters.Add(parameter);
+                }
+            }
+        }
+
+        /// <summary> 
+        /// Ejecuta consultas en la base de datos.
+        /// </summary>
+        /// <param name="query">Consulta a la base de datos</param>
+        /// <param name="parameters">Valores de los parámetros</param>
+        /// <returns>DataSet con los resultados de la consulta o null falla la operación.</returns>
+        public static DataSet Query(string query, List<SqlParameter> parameters = null, bool isSP = false)
+        {
+            try
+            {
+                var cmd = GetCommand(query, parameters, isSP);
+                var adapter = new SqlDataAdapter(cmd);
+                var ds = new DataSet();
+                adapter.Fill(ds);
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// Ejecuta operaciones en la base de datos que no sean de selección.
+        /// </summary>
+        /// <param name="query">Consulta a la base de datos</param>
+        /// <param name="parameters">Valores de los parámetros</param>
+        /// <returns>Cantidad de filas afectadas o null si falla la operación</returns>
+        public static int? NonQuery(string query, List<SqlParameter> parameters)
+        {
+            try
+            {
+                var cmd = GetCommand(query, parameters);
+                cmd.Connection.Open();
+                int affected = cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+                return affected;
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public static DataTable ObtenerTabla(String NombreTabla, String Sql, List<SqlParameter> parameters = null, bool isSP = false)
+        {
+            DataSet ds = new DataSet();
+            SqlCommand cmd = GetCommand(Sql, parameters, isSP);
+            SqlDataAdapter adp = GetAdapter(cmd);
+            adp.Fill(ds, NombreTabla);
+            cmd.Connection.Close();
+            return ds.Tables[NombreTabla];
+        }
+    }
+}
