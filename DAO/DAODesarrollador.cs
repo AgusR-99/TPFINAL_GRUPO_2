3f551f73bb62bbb2c1eecbe12faf868f31fa1719
salvadorzorraquin <salvadorzorraquin@gmail.com>
2022-06-20 19:59:56 -0300

    DAO y Negocio de Desarrollador e Imagen

--- DAO/DAODesarrollador.cs
+++ DAO/DAODesarrollador.cs
@@ -17,5 +17,27 @@ namespace DAO
             return DB.ObtenerTabla("Desarrolladores", "[SP_Desarrolladores_Obtener]", isSP: true);
         }
 
+        public static int? ActualizarDesarrollador(Desarrollador desarrollador)
+        {
+            return DB.NonQuery("[SP_Desarrolladores_Actualizar]", getParametrosDesarrollador(desarrollador, true), true);
+        }
+
+        public static int? AgregarDesarrollador(Desarrollador desarrollador)
+        {
+            return DB.NonQuery("[SP_Desarrolladores_Agregar]", getParametrosDesarrollador(desarrollador, false), true);
+        }
+
+        public static List<SqlParameter> getParametrosDesarrollador(in Desarrollador desarrollador, bool includeID)
+        {
+            var parametros = new List<SqlParameter>()
+            {
+                new SqlParameter("Nombre", desarrollador.getNombre()),
+                new SqlParameter("SitioWeb", desarrollador.getURL_web()),
+                new SqlParameter("UbicacionSede", desarrollador.getUbicacion_sede()),
+                new SqlParameter("Historia", desarrollador.getHistoria())
+            };
+            if (includeID) parametros.Add(new SqlParameter("IdDesarrollador", desarrollador.getID_Desarrollador()));
+            return parametros;
+        }
     }
 }
--- Negocio/NegocioDesarrollador.cs
+++ Negocio/NegocioDesarrollador.cs
@@ -15,5 +15,39 @@ namespace Negocio
         {
             return DAODesarrollador.ListarDesarrolladores();
         }
+
+        public static List<string> ActualizarDesarrollador(Desarrollador desarrollador)
+        {
+            var errorReasons = Validar(desarrollador);
+            if (errorReasons.Any()) return errorReasons;
+
+            int? resultadoActualizar = DAODesarrollador.ActualizarDesarrollador(desarrollador);
+            if (resultadoActualizar == null) errorReasons.Add("Ocurrió un error al actualizar la base de datos");
+            if (resultadoActualizar == 0) errorReasons.Add("No se encontró el registro a actualizar");
+
+            return errorReasons;
+        }
+
+        public static List<string> AgregarDesarrollador(Desarrollador desarrollador)
+        {
+            var errorReasons = Validar(desarrollador);
+            if (errorReasons.Any()) return errorReasons;
+
+            int? resultadoActualizar = DAODesarrollador.AgregarDesarrollador(desarrollador);
+            if (resultadoActualizar == null) errorReasons.Add("Ocurrió un error al actualizar la base de datos");
+            if (resultadoActualizar == 0) errorReasons.Add("No se encontró el registro a actualizar");
+
+            return errorReasons;
+        }
+
+        public static List<string> Validar(in Desarrollador desarrollador)
+        {
+            var errorReasons = new List<string>();
+            if (String.IsNullOrWhiteSpace(desarrollador.getNombre())) errorReasons.Add("El campo Nombre no puede estar vacío");
+            if (String.IsNullOrWhiteSpace(desarrollador.getURL_web())) errorReasons.Add("El campo URL no puede estar vacío");
+            if (String.IsNullOrWhiteSpace(desarrollador.getUbicacion_sede())) errorReasons.Add("El campo Ubicacion no puede estar vacío");
+            if (String.IsNullOrWhiteSpace(desarrollador.getHistoria())) errorReasons.Add("El campo Historia no puede estar vacío");
+            return errorReasons;
+        }
     }
 }
