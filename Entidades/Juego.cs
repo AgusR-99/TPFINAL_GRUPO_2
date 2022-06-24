a7dfeff6587212ec85226ba9a6e4c3c7b53378c0
Pedro <pedrogabe@gmail.com>
2022-06-23 21:03:39 -0300

    Composición en la entidad Juego

--- Entidades/Juego.cs
+++ Entidades/Juego.cs
@@ -10,14 +10,19 @@ namespace Entidades
     {
         private int ID;
         private string Nombre;
-        private int ID_Desarrollador;
         private string Descripcion;
         private bool Activo;
+
+        private Desarrollador Desarrollador;
+        private List<Imagenes> Imagenes;
+        private List<Plataforma> Plataformas;
+        private List<Categoria> Categorias;
+        private List<Tienda> Tiendas;
 
         public Juego(int idjuego, int iddesarrollador, string nombre, string descripcion, bool activo) 
         {
             ID = idjuego;
-            ID_Desarrollador = iddesarrollador;
+            setIDDesarrollador(iddesarrollador);
             Nombre = nombre;
             Descripcion = descripcion;
             Activo = activo;
@@ -25,7 +30,7 @@ namespace Entidades
 
         public Juego(int iddesarrollador, string nombre, string descripcion, bool activo)
         {
-            ID_Desarrollador = iddesarrollador;
+            setIDDesarrollador(iddesarrollador);
             Nombre = nombre;
             Descripcion = descripcion;
             Activo = activo;
@@ -33,14 +38,25 @@ namespace Entidades
 
         public int getID() { return ID; }
         public string getNombre() { return Nombre; }
-        public int getIDDesarrollador() { return ID_Desarrollador; }
+        public int getIDDesarrollador() { return Desarrollador.getID_Desarrollador(); }
         public string getDescripcion() { return Descripcion; }
         public bool getActivo() { return Activo; }
+        public List<Imagenes> getImagenes() { return Imagenes; }
+        public List<Categoria> GetCategorias() { return Categorias; }
+        public List<Plataforma> GetPlataformas() { return Plataformas; }
+        public List<Tienda> GetTiendas() { return Tiendas; }
+        public Desarrollador GetDesarrollador() { return Desarrollador; }
 
         public void setID(int id) { ID=id; }
         public void setNombre(string nombre) {Nombre=nombre; }
-        public void setIDDesarrollador(int ID_desarrollador) {ID_Desarrollador=ID_desarrollador; }
+        public void setIDDesarrollador(int ID_desarrollador) { Desarrollador.setID_Desarrollador(ID_desarrollador); }
         public void getDescripcion(string descripcion) { Descripcion=descripcion; }
         public void getActivo(bool activo) {Activo=activo; }
+
+        public void setImagenes(List<Imagenes> imagenes) { Imagenes = imagenes; }
+        public void setCategorias(List<Categoria> categorias) {  Categorias = categorias; }
+        public void setPlataformas(List<Plataforma> plataformas) {  Plataformas = plataformas; }
+        public void setTiendas(List<Tienda> tiendas) {  Tiendas = tiendas; }
+        public void setDesarrollador(Desarrollador desarrollador) { Desarrollador = desarrollador; }
     }
 }
