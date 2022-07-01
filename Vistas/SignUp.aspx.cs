0681b1500c01bd9b913f256d75539b494520c115
Lucas4d4 <101574260+Lucas4d4@users.noreply.github.com>
2022-07-01 18:59:50 -0300

    Agregado validacion formato Email

--- Vistas/SignUp.aspx.cs
+++ Vistas/SignUp.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using Entidades;
 using Negocio;
+using System.Text.RegularExpressions;
 
 namespace Vistas
 {
@@ -18,7 +19,7 @@ namespace Vistas
 
         protected void btnRegistrar_Click(object sender, EventArgs e)
         {
-            
+            bool valEmail;
            var usuariod = txtUsername.Text;
              var contrasena = txtPassword.Text;
              var email = txtEmail.Text;
@@ -29,42 +30,69 @@ namespace Vistas
              false,
              true
                  );
-            
-                if (txtPassword.Text == txtPassword2.Text)
+            valEmail = ValidarEmail(txtEmail.Text);
+            if (valEmail == true)
             {
+                if (txtPassword.Text == txtPassword2.Text)
+                {
 
-                    
+
                     if (txtPassword.Text.Length >= 8)
                     {
-                    var erroresAgregar = NegocioUsuario.AgregarUsuario(usuario);
-                    if (erroresAgregar == "")
-                    {
-                        lblMsg.ForeColor = System.Drawing.Color.GreenYellow;
-                        lblMsg.Text = "Registro agregado con exito";
-                        txtUsername.Text = "";
-                        txtEmail.Text = "";
-                        txtPassword.Text = "";
-                        txtPassword2.Text = "";
+                        var erroresAgregar = NegocioUsuario.AgregarUsuario(usuario);
+                        if (erroresAgregar == "")
+                        {
+                            lblMsg.ForeColor = System.Drawing.Color.GreenYellow;
+                            lblMsg.Text = "Registro agregado con exito";
+                            txtUsername.Text = "";
+                            txtEmail.Text = "";
+                            txtPassword.Text = "";
+                            txtPassword2.Text = "";
+                        }
+                        else
+                        {
+                            lblMsg.ForeColor = System.Drawing.Color.Red;
+                            lblMsg.Text = erroresAgregar;
+                        }
                     }
                     else
                     {
                         lblMsg.ForeColor = System.Drawing.Color.Red;
-                        lblMsg.Text = erroresAgregar;
+                        lblMsg.Text = "Error. La Contraseña debe tener mas de 7 caracteres";
                     }
-                    }
-                    else
-                    {
-                    lblMsg.ForeColor = System.Drawing.Color.Red;
-                    lblMsg.Text = "Error. La Contraseña debe tener mas de 7 caracteres";
+
                 }
-                
+                else
+                {
+                    lblMsg.ForeColor = System.Drawing.Color.Red;
+                    lblMsg.Text = "Error. No se repitió la contraseña";
+                }
             }
             else
             {
                 lblMsg.ForeColor = System.Drawing.Color.Red;
-                lblMsg.Text = "Error. No se repitió la contraseña";
+                lblMsg.Text = "Error. Email no valido.\n Debe tener el formato: nombre@dominio.com";
             }
-             
+        }
+        private Boolean ValidarEmail(String email)
+        {
+            String expresion;
+            expresion = "\\w+([-+.’]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+            if (Regex.IsMatch(email, expresion))
+            {
+                if (Regex.Replace(email, expresion, String.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
